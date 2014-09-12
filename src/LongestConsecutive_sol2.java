1c79bd46d27378de9ebac8503ceb7ae53df21590
yangscar <yangscar@gmail.com>
2014-09-12 21:44:18 +0800

    Create LongestConsecutive_sol2.java

---
+++ src/LongestConsecutive_sol2.java
@@ -0,0 +1,33 @@
+
+
+public class Solution {
+    public int longestConsecutive(int[] num) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int size = num.length;
+        int len = 1;
+        
+        for (int i = 0; i < size; i++){
+            if (map.containsKey(num[i])) continue;
+            map.put(num[i],1);
+            // union to left old, if left has old
+            if (map.containsKey(num[i]-1))
+                len = Math.max(len, mergeCluster(map, num[i]-1,num[i]));
+            // union to right old, if right has old
+            if (map.containsKey(num[i]+1))
+                len = Math.max(len, mergeCluster(map, num[i],num[i]+1));
+                
+            
+        }
+        return size == 0 ? 0 : len;
+    }
+    
+    private int mergeCluster(HashMap<Integer, Integer> map,
+                                int left, int right){
+        int upper = right + map.get(right) -1;
+        int lower = left - map.get(left) +1;
+        int len = upper - lower +1;
+        map.put(upper,len);
+        map.put(lower,len);
+        return len;
+    }
+}
